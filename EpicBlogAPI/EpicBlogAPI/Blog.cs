f880e4f96fb007eb11ca7efb020ae6ba4f6c76c8
Dawson K <46154365+TheInfinityIQ@users.noreply.github.com>
2022-03-18 14:07:47 -0500

    Added Bogus package to csproj file. Used Bogus to create method to generate blogs. Used FileScope namespace thing

--- EpicBlogAPI/EpicBlogAPI/Blog.cs
+++ EpicBlogAPI/EpicBlogAPI/Blog.cs
@@ -1,22 +1,21 @@
-﻿namespace EpicBlogAPI
-{
-    public class Blog
-    {
-        private static int _uniqueId = 0;
-        public int Id { get; }
-        public DateTime Date { get; }
-        public string Author { get; }
-        public string Title { get; set; }
-        public string Body { get; set; }
+﻿namespace EpicBlogAPI;
 
-        public Blog(string title, string author, string body)
-        {
-            // TODO: Determine how to assign author.UserAccounts?  Or AnonyPost style of manually submitting an author
-            Author = author;
-            Id = _uniqueId++;
-            Date = DateTime.Now;
-            Title = title;
-            Body = body;
-        }
+public class Blog
+{
+    private static int _uniqueId = 0;
+    public int Id { get; }
+    public DateTime Date { get; }
+    public string Author { get; }
+    public string Title { get; set; }
+    public string Body { get; set; }
+
+    public Blog(string title, string author, string body)
+    {
+        // TODO: Determine how to assign author.UserAccounts?  Or AnonyPost style of manually submitting an author
+        Author = author;
+        Id = _uniqueId++;
+        Date = DateTime.Now;
+        Title = title;
+        Body = body;
     }
 }
--- EpicBlogAPI/EpicBlogAPI/DataRepository.cs
+++ EpicBlogAPI/EpicBlogAPI/DataRepository.cs
@@ -1,21 +1,31 @@
-﻿namespace EpicBlogAPI
+﻿using Bogus;
+
+namespace EpicBlogAPI;
+public class DataRepository
 {
-    public class DataRepository
+    public List<Blog> Blogs { get; set; }
+    private Random _rng;
+    private Faker<Blog> _modelFaker;
+
+    public DataRepository()
     {
-        public List<Blog> Blogs { get; set; }
-
-        public DataRepository()
-        {
-            Blogs = new List<Blog>();
-        }
-
-        public void InitializeDb()
-        {
-            string body = "Before you get started posting... , think before posting, be Epic!";
-
-            this.Blogs.Add(new Blog("An EpicBlog Tutorial", "EpicBloggers", body));
-        }
+        Blogs = new List<Blog>();
+        _rng = new Random();
+        _modelFaker = new Faker<Blog>()
+            .RuleFor(o => o.Author, f => f.Name.FirstName() + " " + f.Name.LastName())
+            .RuleFor(o => o.Title, f => f.Random.Words(f.Random.Int(1, 4)))
+            .RuleFor(o => o.Body, f => f.Lorem.Sentences(f.Random.Int(2, 7)));
     }
 
-    
+    public void InitializeDb()
+    {
+        string body = "Before you get started posting... , think before posting, be Epic!";
+
+        this.Blogs.Add(new Blog("An EpicBlog Tutorial", "EpicBloggers", body));
+    }
+
+    public void AddContentToDb(int numBlogsToAdd)
+    {
+        Blogs.AddRange(_modelFaker.Generate(numBlogsToAdd));
+    }
 }
