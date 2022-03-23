d85f17c20524ba9093e7fbc804e9a3ba71608fa0
Dawson K <46154365+TheInfinityIQ@users.noreply.github.com>
2022-03-23 15:47:06 -0500

    Refactored Website HTML and CSS.
    Only index looks as it should. Rest still need to be reworked

--- EpicBlogUI/CRUD/update.js
+++ EpicBlogUI/CRUD/update.js
@@ -2,11 +2,9 @@
 let url = "https://localhost:7061/blog";
 
 const updateRequest = async (event) => {
-    let id = document.getElementById("blogId-update").value;
-    window.location.href = "http://127.0.0.1:5500/useblog.html";
-    //  Prevent from loading until blog is found
     event.preventDefault();
     
+    let id = document.getElementById("blogId-update").value;
     // Get blog
     let blogToEdit = await fetch(url + "/" + id, {method: "GET"}).then((response) => {
         if (response.ok)
@@ -17,33 +15,15 @@ const updateRequest = async (event) => {
         console.log("Update error: " + error.message + ". Returning to index...");
         window.location.href = "http://127.0.0.1:5500/index.html";
     })
-
-    console.log(blogToEdit);
-
-    //TODO: Set values
-    document.getElementById("blog-title").value;
-    document.getElementById("blog-author").value;
-    document.getElementById("blog-body").value;
-
-    // Get informatation and insert it into fields to make it easier to implement smaller changes into a blog
-
-    // fetch(url, {
-    //     method: "PUT",
-    // })
-    //     .then(
-    //         (response) => {
-    //             if (response.ok) {
-    //                 return response.json();
-    //             }
-    //             throw new Error("Request failed!");
-    //         },
-    //         (networkError) => {
-    //             console.log(networkError.message);
-    //         }
-    //     )
-    //     .then((jsonResponse) => {
-    //         console.log(jsonResponse);
-    //     });
+    
+    window.location.replace("http://127.0.0.1:5500/useblog.html");
+    
+    function updatePage () {
+        console.log(blogToEdit);
+        console.log(document.body.innerHTML)
+    }
+    
+    setTimeout(updatePage, 500)
 };
 
 export default updateRequest;
