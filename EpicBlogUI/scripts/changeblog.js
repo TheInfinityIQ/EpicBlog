052d6d9102c6d4dc3f592b45fdd67605528bca1d
Dawson K <46154365+TheInfinityIQ@users.noreply.github.com>
2022-03-29 15:54:40 -0500

    Create Home Button in Nav Bar when read() called

--- EpicBlogUI/scripts/changeblog.js
+++ EpicBlogUI/scripts/changeblog.js
@@ -1,6 +1,8 @@
 import * as blogsModule from "./state.js";
 import * as readModule from "../CRUD/read.js";
 
+let dateOptions = { weekday: "long", year: "numeric", month: "long", day: "numeric" };
+
 const previous = () => {
     let currentIndex = document.getElementById("currentIndex").innerHTML * 1; // Converts the string to a number
 
@@ -12,7 +14,7 @@ const previous = () => {
 
     document.getElementById("blog-title").innerHTML = blogsModule.blogs[currentIndex].title;
     document.getElementById("blog-author").innerHTML = blogsModule.blogs[currentIndex].author;
-    document.getElementById("blog-date").innerHTML = blogsModule.blogs[currentIndex].date;
+    document.getElementById("blog-date").innerHTML = new Date(blogsModule.blogs[currentIndex].date).toLocaleDateString("en-US", dateOptions);
     document.getElementById("blog-body").innerHTML = blogsModule.blogs[currentIndex].body;
     document.getElementById("currentIndex").innerHTML = currentIndex;
     document.getElementById("blog-id").innerHTML = blogsModule.blogs[currentIndex].id;
@@ -29,7 +31,7 @@ const next = () => {
 
     document.getElementById("blog-title").innerHTML = blogsModule.blogs[currentIndex].title;
     document.getElementById("blog-author").innerHTML = blogsModule.blogs[currentIndex].author;
-    document.getElementById("blog-date").innerHTML = blogsModule.blogs[currentIndex].date;
+    document.getElementById("blog-date").innerHTML = new Date(blogsModule.blogs[currentIndex].date).toLocaleDateString("en-US", dateOptions);
     document.getElementById("blog-body").innerHTML = blogsModule.blogs[currentIndex].body;
     document.getElementById("currentIndex").innerHTML = currentIndex;
     document.getElementById("blog-id").innerHTML = blogsModule.blogs[currentIndex].id;
@@ -37,14 +39,30 @@ const next = () => {
 
 const read = async () => {
     let blog = await readModule.readSpecificFunc();
-    
-    document.getElementById("arrow-left").style.display = 'none';
-    document.getElementById("arrow-right").style.display = 'none';
-    document.getElementById("index").style.display= 'none';
 
+    // Nav bar
+    // <nav class="header-nav">
+    //     <button id="nav-update">Update</button>
+    //     <button id="nav-delete">Create</button>
+    //     <button id="nav-log-blogs">LogBlogs</button>
+    // </nav>;
+
+    // Create Home Button
+    let node = document.createElement("button");
+    node.id = "nav-home";
+    let textNode = document.createTextNode("Home");
+    node.appendChild(textNode);
+    document.querySelector(".header-nav").appendChild(node);
+
+    // Remove unneeded elements
+    document.getElementById("arrow-left").style.display = "none";
+    document.getElementById("arrow-right").style.display = "none";
+    document.getElementById("index").style.display = "none";
+
+    //Update blog based on search
     document.getElementById("blog-title").innerHTML = blog.title;
     document.getElementById("blog-author").innerHTML = blog.author;
-    document.getElementById("blog-date").innerHTML = blog.date;
+    document.getElementById("blog-date").innerHTML = new Date(blog.date).toLocaleDateString("en-US", dateOptions);
     document.getElementById("blog-body").innerHTML = blog.body;
     document.getElementById("currentIndex").innerHTML = currentIndex;
     document.getElementById("blog-id").innerHTML = blog.id;
